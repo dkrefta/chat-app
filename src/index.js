dda7b62911e702d361f19af3775946b964880cd7
Diego Krefta <diego.krefta@gmail.com>
2018-07-04 10:44:25 -0300

    Add Chance (dynamic username generator)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import {addUser} from './actions'
-import chat from "./reducers";
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import reducers from './reducers'
+import handleNewMessage from './sagas'
+import setupSocket from './sockets'
+import username from './utils/name'
 
-const store = createStore(chat);
+const sagaMiddleware = createSagaMiddleware()
 
-store.dispatch(addUser('Me'))
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+const socket = setupSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, { socket, username })
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
-);
-
-registerServiceWorker();
+  document.getElementById('root')
+)
+registerServiceWorker()
---
+++ src/utils/name.js
@@ -0,0 +1,4 @@
+import Chance from 'chance'
+
+const chance = new chance()
+export default chance.first()
